a1c0dcf8fb5969c943deb56d26f34afa22fd7aad
Leonardo <leodeveloper12@gmail.com>
2025-02-25 19:57:13 -0300

    continuando o curso de JS

---
+++ Curso JS/Arrays (Avançado)/filter.js
@@ -0,0 +1,7 @@
+//  Filter, map, reduce
+
+const numeros = [5, 50, 10, 50, 8, 2, 65, 11];
+
+const numerosFiltrados = numeros.filter(valor => valor > 10); // Passando uma função callback no Filter
+console.log(numerosFiltrados)
+
---
+++ Curso JS/Arrays (Avançado)/splice.js
@@ -0,0 +1,7 @@
+
+const nomes = ["Eduardo", "Maria", "Joana", "Wallace", "Rosana"];
+
+// nomes.splice(indice, delete, elem1, elem2);
+const removidos = nomes.splice(4, 1, "Luiz");
+console.log(nomes, removidos);
+
